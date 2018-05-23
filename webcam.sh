116593ab0161a290cda4cf0cadc38ac3123c6b36
Riikka Schroderus <schroderusriikka@gmail.com>
2018-05-23 20:20:09 +0300

    add command concerning index.html

--- webcam.sh
+++ webcam.sh
@@ -2,12 +2,14 @@
 
 # replace gs://raspberry_pi_riikka below with
 # your own bucket name.
-gsutil index.html
+BUCKET_NAME=raspberry_pi_riikka
+
+gsutil cp index.html gs://$BUCKET_NAME
 
 while true
 do
 raspistill -w 640 -h 480 15cm -q 75 -o image.jpg
-gsutil -h "Cache-Control:no-cache" cp image.jpg gs://raspberry_pi_riikka
-gsutil acl ch -u AllUsers:R gs://raspberry_pi_riikka/image.jpg
+gsutil -h "Cache-Control:no-cache" cp image.jpg gs://$BUCKET_NAME
+gsutil acl ch -u AllUsers:R gs://$BUCKET_NAME/image.jpg
 sleep 1m
 done
